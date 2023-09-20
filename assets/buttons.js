63426fedc0940c6f847bf03164b2a1cf903beafd
hangever147 <107249134+hangever147@users.noreply.github.com>
2023-09-20 18:26:39 +0800

    Update buttons.js

--- assets/buttons.js
+++ assets/buttons.js
@@ -1,3 +1,4 @@
+
 require(['gitbook', 'jquery'], function(gitbook, $) {
 
     gitbook.events.bind('start', function(e, config) {
@@ -7,7 +8,14 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
                 'icon': 'fa fa-globe',
                 'onClick': function(e) {
                     e.preventDefault();
-                    window.open(config.i18n.EnglishUrl, '_self');
+                    var URL= GetUrlRelativePath();
+                    var newUrl ='';
+                    if (URL.includes('/zh-CN/')){
+                        newUrl=URL.replace('/zh-CN/', config.i18n.EnglishUrl);
+                    }else {
+                        newUrl=URL;
+                    }
+                    window.open(newUrl, '_self');
                 }
             },
             'chinese': {
@@ -15,7 +23,14 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
                 'icon': 'fa fa-language',
                 'onClick': function(e) {
                     e.preventDefault();
-                    window.open(config.i18n.ChineseUrl, '_self');
+                    var URL= GetUrlRelativePath();
+                    var newUrl ='';
+                    if (URL.includes('/en-US/')){
+                        newUrl=URL.replace('/en-US/', config.i18n.ChineseUrl);
+                    }else {
+                        newUrl=URL;
+                    }
+                    window.open(newUrl, '_self');
                 }
             }
         };
@@ -42,3 +57,16 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
 
     });
 });
+
+function GetUrlRelativePath() {
+    var url = document.location.toString();
+    var arrUrl = url.split('//');
+
+    var start = arrUrl[1].indexOf('/');
+    var relUrl = arrUrl[1].substring(start);// stop省略，截取从start开始到结尾的所有字符
+
+    if(relUrl.indexOf('?') != -1){
+        relUrl = relUrl.split('?')[0];
+    }
+    return relUrl;
+}
